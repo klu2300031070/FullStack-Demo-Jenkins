5ab9a2ff6b86df8672d007ff741997a1d9f8bc73
Kamesh <2300031070@kluniversity.in>
2025-09-05 15:20:12 +0530

    Updatd 3.0

--- BACKEND/src/main/java/com/my/demo/controller/StudentController.java
+++ BACKEND/src/main/java/com/my/demo/controller/StudentController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -24,7 +25,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @CrossOrigin(origins = "*")
-public class StudentController {
+public class StudentController extends SpringBootServletInitializer {
 	@Autowired
 	private StudentRepo r; 
 
